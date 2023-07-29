e4835681166c42ca809624253cdb7630f0d460b8
Helmi <helmi.akbar@phincon.com>
2023-07-29 15:03:58 +0700

    Initial Commit

---
+++ EmployeeList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmployeeList
+//
+//  Created by PT Phincon on 29/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
